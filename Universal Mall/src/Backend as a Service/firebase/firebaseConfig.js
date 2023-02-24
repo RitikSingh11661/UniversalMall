fbc56f6a3339b06f3fa9c0ff78b0c3295860bea5
Ritik Singh <delhi8260@gmail.com>
2023-02-24 10:01:45 +0530

    changesInday_2

--- Universal Mall/src/Backend as a Service/firebase/firebaseConfig.js
+++ Universal Mall/src/Backend as a Service/firebase/firebaseConfig.js
@@ -23,8 +23,7 @@
 
 // const listAllUsers = (nextPageToken) => {
 //   // List batch of users, 1000 at a time.
-//   getAuth()
-//     .listUsers(1000, nextPageToken)
+//   getAuth().listUsers(1000, nextPageToken)
 //     .then((listUsersResult) => {
 //       listUsersResult.users.forEach((userRecord) => {
 //         console.log('user', userRecord.toJSON());
