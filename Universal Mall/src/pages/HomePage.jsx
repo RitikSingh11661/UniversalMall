081cecd48de984762726f1bfecad003e4ce9ae13
AkashDasmondal94 <akash.dasmondal@gmail.com>
2023-02-25 16:17:57 +0530

    update

--- Universal Mall/src/pages/HomePage.jsx
+++ Universal Mall/src/pages/HomePage.jsx
@@ -3,11 +3,13 @@ import Brand from './Brand'
 import Footer from './Footer'
 import Grocery from './Grocery'
 import Jewellery from './Jewellery'
+import Navbar from './Navbar'
 
 
 function HomePage() {
   return (
     <div>
+      <Navbar/>
       <Brand/>
       <Grocery/>
       <Jewellery/>
